ae09bfac0ab2706c3ed6eb79e9eba64685ca4325
panky-codes <pankaj.sarathy1992@gmail.com>
2021-03-16 09:12:54 +0100

    Day 10 not completed

--- 2020/day10/src/main.rs
+++ 2020/day10/src/main.rs
@@ -44,38 +44,67 @@ const _RAW_INP2: &str = r"16
 4";
 
 fn puzzle_one(inp_vec: &Vec<u32>) {
-    let one_offset = if inp_vec[0] == 1 { 1 } else { 0 };
-    let three_offset = if inp_vec[0] == 3 { 1 } else { 0 };
+    // let one_offset = if inp_vec[1] == 1 { 1 } else { 0 };
+    // let three_offset = if inp_vec[1] == 3 { 1 } else { 0 };
 
-    let val = inp_vec.iter();
-    let next_val = inp_vec.iter().skip(1);
-
-    let adj_diff: Vec<u32> = val.zip(next_val).map(|(cur, next)| next - cur).collect();
-
-    let count_one: usize = adj_diff.iter().filter(|x| **x == 1).count() + one_offset; // Plus one for the 0th case
-    let count_three: usize = adj_diff.iter().filter(|x| **x == 3).count() + three_offset + 1; // Plus one for last charger
+    let (count_one, count_three) = inp_vec.windows(2).fold(
+        (0, 0), // Plus one for the last charger
+        |(count_one, count_three), x| match x[1] - x[0] {
+            1 => (count_one + 1, count_three),
+            3 => (count_one, count_three + 1),
+            _ => unreachable!(),
+        },
+    );
 
     println!("Puzzle 1 ans is {:?}", count_one * count_three);
 }
 
-// Works for the first two sample inputs but it does not 
+// Works for the first two sample inputs but it does not
 // scale for the main input
-fn puzzle_two(inp_vec: &Vec<u32>, val: u32) -> u32 {
+fn _puzzle_two_recur(inp_vec: &Vec<u32>, val: u32) -> u32 {
     let mut count: u32 = 0;
 
     if val == inp_vec[inp_vec.len() - 1] {
+        print!(".");
         return 1;
     }
     if inp_vec.contains(&(val + 1)) {
-        count += puzzle_two(&inp_vec, val + 1);
+        count += _puzzle_two_recur(&inp_vec, val + 1);
     }
     if inp_vec.contains(&(val + 3)) {
-        count += puzzle_two(&inp_vec, val + 3);
+        count += _puzzle_two_recur(&inp_vec, val + 3);
     }
     if inp_vec.contains(&(val + 2)) {
-        count += puzzle_two(&inp_vec, val + 2);
+        count += _puzzle_two_recur(&inp_vec, val + 2);
     }
     count
+}
+
+fn puzzle_two(inp_vec: &Vec<u32>) -> u64 {
+    // A big Vector (like a hashmap) with index used as the charger joltage and value corresponds
+    // to the nr. of possible ways to reach it
+    // TODO: Change it to hashmap to save space
+    let mut conn_count: Vec<u64> = vec![0; (inp_vec.last().unwrap() +1) as usize];
+
+    conn_count[0] = 1; // The first adapter is the starting point
+
+    for (index, val) in inp_vec.iter().skip(1).enumerate() {
+        // Skip the first element as it is the first starting point
+        if inp_vec.contains(&(*val - 1)) {
+            conn_count[*val as usize] += conn_count[(*val - 1) as usize];
+        }
+        if (*val as i32 - 2) > 0 {
+            if inp_vec.contains(&(*val - 2)) {
+                conn_count[*val as usize] += conn_count[(*val - 2) as usize];
+            }
+        }
+        if (*val as i32 - 3) > 0 {
+            if inp_vec.contains(&(*val - 3)) {
+                conn_count[*val as usize] += conn_count[(*val - 3) as usize];
+            }
+        }
+    }
+    *conn_count.last().unwrap()
 }
 
 fn main() {
@@ -84,10 +113,15 @@ fn main() {
     let inp = contents;
     let mut inp_vec: Vec<u32> = inp.split('\n').map(|s| s.parse::<u32>().unwrap()).collect();
     // First puzzle
-    inp_vec.sort();
+    inp_vec.insert(0, 0);
+
+    inp_vec.sort_unstable();
+
+    inp_vec.push(*inp_vec.last().unwrap() + 3);
+
     puzzle_one(&inp_vec);
 
     // Second puzzle
-    // let count = puzzle_two(&inp_vec, 0);
-    // println!("{}", count);
+    let count = puzzle_two(&inp_vec);
+    println!("{}", count);
 }
